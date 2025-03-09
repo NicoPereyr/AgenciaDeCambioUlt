b1558d7b620e92828a362c436fb6f261cf002141
Nico Pereyr <nicolaspereyra33@gmail.com>
2025-03-08 22:56:44 -0300

    Carga de la lista de divisas

--- AgenciaDeCambioPOO.Windows/frmVentaDivisa.cs
+++ AgenciaDeCambioPOO.Windows/frmVentaDivisa.cs
@@ -1,13 +1,18 @@
-﻿namespace AgenciaDeCambioPOO.Windows
+﻿using AgenciaDeCambioPOO.Datos;
+using AgenciaDeCambioPOO.Entidades;
+using AgenciaDeCambioPOO.Windows.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgenciaDeCambioPOO.Windows
 {
     public partial class frmVentaDivisa : Form
     {
-        private readonly IServiceProvider serviceProvider;
+        private readonly IServiceProvider _serviceProvider;
 
         public frmVentaDivisa(IServiceProvider serviceProvider)
         {
             InitializeComponent();
-            this.serviceProvider = serviceProvider;
+            this._serviceProvider = serviceProvider;
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -18,6 +23,9 @@
         private void frmVentaDivisa_Load(object sender, EventArgs e)
         {
             txtFechaOperacion.Text = DateTime.Now.ToShortDateString();
+            AgenciaDeCambio agencia = _serviceProvider.GetService<AgenciaDeCambio>()!;
+            //Lista de divisa
+            ComboHelper.CargarDatosCombo<Divisa>(ref cboDivisas, agencia.ObtenerDivisas(), "Nombre", "Abreviatura");
         }
     }
 }
